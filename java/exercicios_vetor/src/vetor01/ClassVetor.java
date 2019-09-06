ef943f99bc14fedaec5d2c6f67bd79b5bb19b63e
aldrinjunior <aldrinjunior@gmail.com>
2019-09-05 22:48:48 -0300

    arquivo contator nodos-arvore

---
+++ java/exercicios_vetor/src/vetor01/ClassVetor.java
@@ -0,0 +1,15 @@
+package vetor01;
+import java.util.Scanner;
+public class ClassVetor {
+
+    public static void main (String [] args){
+            Scanner scan = new Scanner(System.in);
+            int tam=0, i=0;
+            int vetor[] = new int[tam];
+            System.out.println("Digite o tamanho do vetor: ");
+            tam = scan.nextInt();
+            System.out.println("O tamanho do vetor é: "+i);
+
+        }
+    }
+
